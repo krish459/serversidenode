36e852e80a52c940a6cd34fee990fb893ac9b725
Krish <krishshah235@gmail.com>
2021-12-30 16:57:28 +0530

    made rectangle module

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var Rect = {
-    perimeter : (x,y) =>(2*(x+y)),
-    area : (x,y) =>(x*y)
-};
+// var Rect = {
+//     perimeter : (x,y) =>(2*(x+y)),
+//     area : (x,y) =>(x*y)
+// };
+
+var Rect = require('./rectangle');
 
 function solveRect(l,b){
     console.log("solving for Rectangle with l = "+l+"and b = "+b);
